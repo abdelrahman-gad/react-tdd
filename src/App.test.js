7f784d95e8983df8ff78221b03c35f3f81e7c59a
abdelrahman-gad <abdelrahmangad95@gmail.com>
2021-03-08 23:17:28 +0200

    adding setup imports to setupTest.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-import { configure , shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 
-configure({ adapter: new Adapter() });
 describe('Counter Testing ', () => {
  
   let wrapper; 
